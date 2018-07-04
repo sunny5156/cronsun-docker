a8cef2072cc69f354b5c8014b05322e73b85adbe
sunny5156 <sunny5156@qq.com>
2018-07-04 14:25:59 +0800

    增加 cronnode cronweb的启动脚本

---
+++ config/cronsun/cronnode_start.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+BASE_DIR=/worker/cronsun
+
+${BASE_DIR}/cronnode -conf /${BASE_DIR}/conf/base.json
---
+++ config/cronsun/cronweb_start.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+BASE_DIR=/worker/cronsun
+
+${BASE_DIR}/cronweb -conf /${BASE_DIR}/conf/base.json
